fe5840e728eabe9999c45e04d513c2860ba37581
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-03-08 16:54:16 +0300

    first part in async in process

---
+++ async_function_and_coroutines/3.2_coroutine_type.py
@@ -0,0 +1,27 @@
+import asyncio
+import time
+
+
+async def func1(x):
+    print(x ** 2)
+    await asyncio.sleep(3)
+    print('func1 ended')
+
+
+async def func2(x):
+    print(x ** 0.5)
+    await asyncio.sleep(3)
+    print('func2 ended')
+
+
+async def main():
+    task1 = asyncio.create_task(func1(4))
+    task2 = asyncio.create_task(func2(4))
+
+    await task1
+    await task2
+
+
+print(type(func1))
+
+print(type(func1(4)))
---
+++ first_async_app/2.1_sync.py
@@ -0,0 +1,28 @@
+import time
+
+
+def func1(x):
+    print(x ** 2)
+    time.sleep(3)
+    print('func1 ended')
+
+
+def func2(x):
+    print(x ** 0.5)
+    time.sleep(3)
+    print('func2 ended')
+
+
+def main():
+    func1(4)
+    func2(4)
+
+
+print(time.strftime('%X'))
+
+
+main()
+
+
+print(time.strftime('%X'))
+
